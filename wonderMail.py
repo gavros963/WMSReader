88f6c759f1c1469b43fc264fddd62c1bca609076
BuildTools <unconfigured@null.spigotmc.org>
2025-08-04 22:44:24 -0500

    ported bitsToStructure and WMSStrut

--- wonderMail.py
+++ wonderMail.py
@@ -29,6 +29,28 @@ byteSwap = [
 		0x0C, 0x19
 	]
 
+
+WMSStruct = [
+	{"name":"nullBits", "note":"Null bits", "size":8},
+	{"name":"specialFloor", "note":"Special floor marker", "size":8},
+	{"name":"floor", "note": "Floor", "size":8},
+	{"name":"dungeon", "note": "Dungeon", "size":8},
+	{"name":"flavorText", "note": "Modifies the flavor text", "size":24},
+	{"name":"restriction", "note": "Restriction data", "size":11},
+	{"name":"restrictionType", "note": "Restriction type; mon = 1, type = 0", "size":1},
+	{"name":"reward", "note": "Reward", "size":11},
+	{"name":"rewardType", "note": "Reward type", "size":4},
+	{"name":"targetItem", "note": "Target item", "size":10},
+	{"name":"target2", "note": "Additional target Poke for certain mission types", "size":11},
+	{"name":"target", "note": "Target Poke", "size":11},
+	{"name":"client", "note": "Client Poke", "size":11},
+	{"name":"missionSpecial", "note": "Mission special texts", "size":4},
+	{"name":"missionType", "note": "Mission type", "size":4},
+	{"name":"mailType", "note": "Mail type marker (must be 0100 = 4)", "size":4},
+	{"name":"checksum", "note":"checksum", "size":32, "noinclude":True}
+]
+
+
 def sanitize(wmString):
 	wmString = wmString.upper()
 	outstring = ''
@@ -161,6 +183,30 @@ def decryptbitstream(curbitstream, encrypt=False):
 	outString += skyChecksumBits + checksumBits
 
 	return outString
+
+
+
+def bitsToStructure(bitString):
+	bitPtr = 0
+	outputStrut = {}
+	outputStrutBit = {}
+
+
+	for strutInfo in WMSStruct:
+		bitData = bitString[bitPtr:strutInfo['size']]
+		bitPtr += strutInfo['size']
+
+
+		numData = int(bitData, base=2)
+
+		outputStrut[strutInfo['name']] = numData
+		outputStrutBit[strutInfo['name']] = bitData
+	
+	if bitPtr != bitString.len:
+		print(f"WARNING: Not all available data was parsed into struct. Final bitPtr is {bitPtr}, length is {bitString.len}")
+
+	return outputStrut
+
 
 
      
